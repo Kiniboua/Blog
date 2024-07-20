0672af6f380ff50c84d3f2341c6d3280059fb152
Kiniboua <olivierkiniboua99@gmail.com>
2024-07-20 09:04:15 +0100

    Style Django Forms With Bootstrap

--- theblog/forms.py
+++ theblog/forms.py
@@ -5,3 +5,9 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ['title', 'title_tag', 'body']
+
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Title'}),
+            'title_tag': forms.TextInput(attrs={'class':'form-control', 'placeholder': 'Title Tag'}),
+            'body': forms.Textarea(attrs={'class':'form-control', 'placeholder': 'Body'}),
+        }
